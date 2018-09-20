587b8273c471cb58836c5d8ea8f0a0de82fe3bbf
Pallavi Agarwal <36764894+pallavimaruji@users.noreply.github.com>
2018-09-20 18:37:24 +0530

    Add files via upload

---
+++ Difference of max and min prime no.s/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    return 0;
+}
+found=0;
+    for(i=startval;i<=endval;i++)
+    {
+        for(i=2;i<=n/2;i++)
+        {
+            if(startval%i==0)
+                found=1;
+            if(found==0)
